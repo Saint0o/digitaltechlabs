a95db4e256aab422073f8bfe63d4ff1d999cae11
Расулов Теймур Эминович <terasulov@vtb.ru>
2021-09-17 21:41:34 +0300

    first commit

---
+++ src/main/java/com/digitaltechlabs/digitaltechlabs/DigitaltechlabsApplication.java
@@ -0,0 +1,13 @@
+package com.digitaltechlabs.digitaltechlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitaltechlabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitaltechlabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitaltechlabs/digitaltechlabs/DigitaltechlabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitaltechlabs.digitaltechlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitaltechlabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
